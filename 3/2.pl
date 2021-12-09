9288ba7e723731562b2077d666a0a0e830f0bb79
Hudson <gdog2u@gmail.com>
2021-12-08 21:48:25 -0500

    Remove commented out debug lines

--- 3/2.pl
+++ 3/2.pl
@@ -18,9 +18,6 @@ my $o2_gen_rate = find_o2_gen_rate($first_mcb, 1, \@first_mcb_filter);
 my $co2_scrub_rate = find_co2_scrub_rate($first_lcb, 1, \@first_lcb_filter);
 
 # Print solution
-# debug(sprintf('%b', $o2_gen_rate));
-# debug(sprintf('%b', $co2_scrub_rate));
-
 print $o2_gen_rate * $co2_scrub_rate;
 
 sub find_o2_gen_rate
@@ -29,15 +26,10 @@ sub find_o2_gen_rate
 	my $current_bit = $_[1];
 	my @search_space = @{ $_[2] };
 
-	# print "\n\n\n";
-
-	# debug("Step: ", $current_bit);
-
 	if(scalar @search_space == 1){ return oct "0b".$search_space[0]; }
 	elsif(scalar @search_space == 0){ die "Uh oh, you made a fucky wucky\n\tLast \$query: $query\n"; }
 
 	my $mcb = find_mcb_in_position($current_bit, \@search_space);
-		# debug($mcb);
 	$query .= $mcb;
 
 	@search_space = grep(/^$query/, @search_space);
@@ -61,8 +53,6 @@ sub find_co2_scrub_rate
 
 	return find_co2_scrub_rate($query, ++$current_bit, \@search_space);
 }
-
-
 
 # Find the most common bit at a given index of an array of bytes
 sub find_mcb_in_position
@@ -89,8 +79,6 @@ sub find_mcb_in_position
 		if($current_bit eq '0'){ $low++; }
 		else{ $high++; }
 	}
-
-	# debug($high, $low);
 
 	return $high == $low ? clamp($high) : clamp($high - $low);
 }
